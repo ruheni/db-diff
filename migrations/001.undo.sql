3ded32c30eb08e8505def6bdbcc8791e6845318e
ruheni <aleruheni@outlook.com>
2022-10-14 18:39:03 +0200

    delete unused files

--- migrations/001.undo.sql
+++
@@ -1,8 +0,0 @@
--- DropForeignKey
-ALTER TABLE "Post" DROP CONSTRAINT "Post_authorId_fkey";
-
--- DropTable
-DROP TABLE "User";
-
--- DropTable
-DROP TABLE "Post";
--- out.sql
+++
@@ -1,9 +0,0 @@
--- DropTable
-PRAGMA foreign_keys=off;
-DROP TABLE "User";
-PRAGMA foreign_keys=on;
-
--- DropTable
-PRAGMA foreign_keys=off;
-DROP TABLE "Post";
-PRAGMA foreign_keys=on;
